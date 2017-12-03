c7729dd72e6ee60fa4c0a2c1330900c965d21495
Nuno Lages <nuno@Hermes.lan>
2017-12-03 17:34:48 +0000

    line chart working for a single account

--- CashViz.js
+++ CashViz.js
@@ -1,3 +1,10 @@
+// TEMPORARY VARIABLES
+var gncaPath = '20171202_110643_gnucash_export_Livro_1.gnca';
+var someAccountIds = ['4d849998e7fee25043cbc86af06e7fc0', '1423ea7c1319b7e1a507d2cef8c6e610', 'e9472a3ad501e82750423eff1cecdae8'];
+
+
+
+
 const debug = false; // TODO debug output must be sent to a file, otherwise the browser goes insane
 
 const regexRational = /-?\d+\/\d+/;
@@ -27,9 +34,6 @@ const positiveAccountTypes = [ASSET, BANK, MUTUAL, CASH],
     goodAccounts = [ASSET, BANK, MUTUAL, CASH, EQUITY],
     badAccounts = [EXPENSE, LIABILITY, CREDIT];
 //neutralAccounts = [Adjustments, Initial];
-
-var gncaPath = '20171202_110643_gnucash_export_Livro_1.gnca';
-// var gncaPath = 'data.xml';
 
 var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S %Z");
 var parseDateYearMonth = d3.timeParse("%Y-%m");
@@ -207,10 +211,66 @@ function monthlyTotalProfit(accountsObject, transactionsObject) {
 }
 
 
-function accountTimeLines() {
+// Draws a line chart that shows the amount in each account over time
+// receives an array of account ids
+function accountTimeLines(accountsObject, transactionsObject, accountIds) {
 
+    // If accountIds is empty, null or undefined, draw all accounts
 
+    // set the dimensions and margins of the graph
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
+    // set the ranges
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLinear().range([height, 0]);
+
+    // define the line
+    var valueline = d3.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.partial); });
+
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Get the data
+    var data = [],
+        partial = 0;
+    for (var trnId in transactionsObject) {
+        transactionsObject[trnId].splits.forEach(split => {
+            if (split.account === someAccountIds[0]) {
+                partial += split.quantity;
+                data.push({ "date": transactionsObject[trnId].datePosted, "partial": partial });
+            }
+        });
+    }
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.partial; })]);
+
+    // Add the valueline path.
+    svg.append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d", valueline);
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Add the Y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
 }
 
 
@@ -221,6 +281,8 @@ d3.xml(gncaPath, parseGnca);
 function main(accountsObject, transactions) {
 
     // Make a bar chart
-    monthlyTotalProfit(accountsObject, transactions);
+    // monthlyTotalProfit(accountsObject, transactions);
+
+    accountTimeLines(accountsObject, transactions, someAccountIds);
 
 };
